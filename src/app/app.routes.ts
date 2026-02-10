c570c9d85a501a12f309063eb5951497f565ae5c
simone richelle <simonerichelle4@gmail.com>
2026-02-10 12:23:54 +0100

    composants et configuring des routes du repo vendors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
+import { Cart } from './pages/cart/cart';
+import { Catalog } from './pages/catalog/catalog';
+import { Checkout } from './pages/checkout/checkout';
+import { Login } from './pages/login/login';
+import { Orders } from './pages/orders/orders';
+import { Product } from './pages/product/product';
+import { Profile } from './pages/profile/profile';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'catalog', pathMatch: 'full'},
+    { path: 'cart', component: Cart },
+    { path: 'catalog', component: Catalog },
+    { path: 'checkout', component: Checkout },
+    { path: 'login', component: Login },
+    { path: 'orders', component: Orders },
+    { path: 'product', component: Product },
+    { path: 'profile', component: Profile },
+    { path: 'register', component: Register }
+];
